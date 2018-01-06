c6f4534bce9fc7b435fec149dc26b8a0a02d1322
KENNYSOFT <525hm@naver.com>
2018-01-07 02:56:02 +0900

    Implement subtract

---
+++ src/kr/KENNYSOFT/SEHomework/SETest/SubtractorTest.java
@@ -0,0 +1,31 @@
+package kr.KENNYSOFT.SEHomework.SETest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import kr.KENNYSOFT.SEHomework.Operator.Operator;
+import kr.KENNYSOFT.SEHomework.Operator.Subtractor;
+
+public class SubtractorTest
+{
+	@Test
+	public void testSubtractNormalInt()
+	{
+		Operator subtractor = new Subtractor();
+		Assert.assertEquals(subtractor.operate(1, 2), "-1");
+	}
+
+	@Test
+	public void testSubtractLargeInt()
+	{
+		Operator subtractor = new Subtractor();
+		Assert.assertEquals(subtractor.operate(2147483647, 2100000000), "47483647");
+	}
+
+	@Test
+	public void testSubtractLargeString()
+	{
+		Operator subtractor = new Subtractor();
+		Assert.assertEquals(subtractor.operate("9223372036854775808", "18446744073709551616"), "-9223372036854775808");
+	}
+}
